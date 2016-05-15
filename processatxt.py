8262985ca1d0626054200b384f8018e5341ca78c
Alexandre Gomes <alegomes@gmail.com>
2016-05-15 11:58:51 -0300

    quantos caracteres tem em cada linha?

--- processatxt.py
+++ processatxt.py
@@ -3,5 +3,7 @@ import json
 f = open('50linhasfd.txt')
 for l in f:
 	# print(l, end='')
-	obj = obj = json.loads(l)
-	print(obj['id'])
+	print('A string tem',len(l),'caracteres.')
+	if l:
+		obj = obj = json.loads(l)
+		print(obj['id'])
